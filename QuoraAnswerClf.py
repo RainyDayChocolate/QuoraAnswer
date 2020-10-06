ca65a8ad9b8cd6107e8fcb724c91731f1eeda129
RainyDayChocolate <likuangzheng@hotmail.com>
2020-10-05 21:49:25 -0400

    add basic code

--- QuoraAnswerClf.py
+++ QuoraAnswerClf.py
@@ -7,14 +7,21 @@ class AnswerClassifier:
     def __init__(self):
 
         self._model = None
-        self.useful_features = [str(i) for i in range(1, 22)]
-
-    def preprocess_feature(self, features_strings):
+        self.useful_features = set([i for i in range(1, 22)])
+        self.useful_features.remove(19)
+        self.pre_processer = sklearn.
+    def preprocess_feature(self, features_string):
         features = []
-        for feature in features_strings:
+        for feature in features_string:
             _id, val = feature.split(':')
             _id, val = eval(_id), eval(val)
             if _id < 23:
+                if _id == 1:
+                    val = val ** (1 / 4)
+                if _id == 12:
+                    val = val if val < 10 else 10
+                if _id == 20:
+                    val = val if val < 20 else 20
                 features.append(val)
         return features
 
@@ -22,6 +29,14 @@ class AnswerClassifier:
         record = record.split(' ')
         y = eval(record[1])
         y = y > 0
-        for 
+        X = self.preprocess_feature(record[2:])
+        return X, y
+
+    def preprocess_test_data(self, record):
+        record = record.split(' ')
+        X = self.preprocess_feature(record[1:])
+
+
+
 
 
